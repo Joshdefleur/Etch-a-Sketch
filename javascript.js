3096938707967f72593bef3a43b662b162e530e4
Josh de Fleuriot <josh.defleuriot@gmail.com>
2024-12-07 12:54:00 +0200

    grid functions

--- javascript.js
+++ javascript.js
@@ -2,26 +2,52 @@
 //grid creator
 
 const container = document.querySelector(".grid");
+const slider = document.getElementById("sliderSize");
+const sliderOutput = document.getElementById("sizeValue");
+const reset = document.getElementById("reset");
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
-  };
-};
 
-makeRows(4, 4);
+slider.addEventListener("change", () => { // This is the slider event listener
+  let squares = slider.value;
+  
+  container.innerHTML ="";
+
+  container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+
+  for (let i = 0; i < (squares * squares); i++) {
+      let square = document.createElement('div');
+      square.classList.add("square");
+      container.appendChild(square);
+  }
+
+  const gridCells = document.querySelectorAll(".square");
+
+  gridCells.forEach(cell => { // This is how the new property is added to grey-out a square after a square is hovered over
+      cell.addEventListener("mouseover", () => {
+          cell.classList.add("hov-square");
+      });
+  });
+
+  reset.addEventListener("click", () => { // This was the reset button I used previously, kept it here to let you see what I did to make it
+      gridCells.forEach(cell => {
+          cell.classList.remove("hov-square");
+          container.removeChild(cell);
+      });
+  });
+
+});
 
 //text changing
 
 
-const slider = document.getElementById("sliderSize");
-const sliderOutput = document.getElementById("sizeValue");
 
 sliderOutput.value = slider.value;
 
 slider.oninput = function(){
     sliderOutput.value = `${this.value} x ${this.value} `;
 }
+
+
+//slider event listener
+
